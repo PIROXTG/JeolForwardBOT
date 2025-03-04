808c46c682c324cb176d8e08235adfa77dbd97aa
PIRO <143400450+PIROXTG@users.noreply.github.com>
2025-03-04 10:04:17 +0530

    ok

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("DQTheFileDonor")
+    return web.json_response("Thanks For Supporting. Its Your PiRO TG: @piroxbots & @piro_files")
